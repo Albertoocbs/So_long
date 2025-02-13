5507837b8d5ba28c7dd0a7b58fc3d07de790c5fe
Alberto Outumuro Bueno <aoutumur@c2r6s1.42lausanne.ch>
2025-02-13 09:36:02 +0100

    retouches avant eval

--- error_exit.c
+++ error_exit.c
@@ -6,7 +6,7 @@
 /*   By: aoutumur <aoutumur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 13:06:58 by aoutumur          #+#    #+#             */
-/*   Updated: 2025/02/12 13:08:02 by aoutumur         ###   ########.fr       */
+/*   Updated: 2025/02/13 09:32:33 by aoutumur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,16 @@
 
 void	free_and_exit(t_data *data)
 {
+	if (data->wall_img)
+		mlx_destroy_image(data->mlx_ptr, data->wall_img);
+	if (data->player_img)
+		mlx_destroy_image(data->mlx_ptr, data->player_img);
+	if (data->collectible_img)
+		mlx_destroy_image(data->mlx_ptr, data->collectible_img);
+	if (data->floor_img)
+		mlx_destroy_image(data->mlx_ptr, data->floor_img);
+	if (data->exit_img)
+		mlx_destroy_image(data->mlx_ptr, data->exit_img);
 	if (data->map)
 		free_map(data->map, data->rows);
 	if (data->mlx_ptr)
